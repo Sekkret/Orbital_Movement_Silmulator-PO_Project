713a4f689f159ae4bcee0452ae91f1164b8ba287
Hoafrost <81311014+Hoafrost@users.noreply.github.com>
2021-05-04 23:10:13 +0200

    ok

--- ProjektPO/src/Whiteboard/AnimationManager.java
+++ ProjektPO/src/Whiteboard/AnimationManager.java
@@ -26,12 +26,6 @@ public class AnimationManager implements Runnable {
 			SwingUtilities.invokeLater(new Runnable(){
 				@Override
 				public void run() {
-					System.out.println(cons.newfi);
-					cons.newfi += cons.L/cons.mi/cons.newr/cons.newr*100;
-					cons.newr = (cons.P/(1 + cons.epsilon * Math.cos(cons.newfi + Math.acos((cons.P-cons.r_p)/cons.epsilon/cons.r_p)-cons.fi_0)));
-					cons.frame.getWhiteboardPanel().x =cons.newr*Math.cos(cons.newfi);
-					cons.frame.getWhiteboardPanel().y =cons.newr*Math.sin(cons.newfi);	
-					cons.frame.getWhiteboardPanel().repaint();
 					cons.frame.getWhiteboardPanel().repaint();
 					cons.frame.getInputPanel().getxValueInput().setText(Double.toString(x)); 
 					cons.frame.getInputPanel().getyValueInput().setText(Double.toString(y)); 
